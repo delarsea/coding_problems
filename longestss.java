3ff1a671adb8829a2e9a17c6900f48c70e05f01c
Oliver Turnbull <olli.turnbull@gmail.com>
2020-12-11 16:14:15 +0000

    more coding problems

---
+++ longestss.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0; // storing legth of longest contiguous string
+        int i = 0; // start pointer 
+        int j = 0; // end pointer
+        int n = s.length()
+        while (i < n && j < n) {
+            
+        }
+        
+    }   
+    public boolean isUniqueChars(String s, int start, int end){
+        Set<Character> letters = new HashSet<>();
+        for (int i = start, i < end, i++) {
+            Character ch = s.charAt(i);
+            
+        }
+    }
+}
